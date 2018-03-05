c581ffe25c0ee02401367ea5c890051c53bc9142
mohafizz <mohafizz@gmail.com>
2018-03-05 21:49:18 +0800

    upload solution for todo-list

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-tasks = ["buy milk", "eat dinner", "nail javascript"]
+var tasks = ["buy milk", "eat dinner", "nail javascript"];
+var todoList = document.getElementById("todo-list");
+
+for (let i = 0; i < tasks.length; i++) {
+  createList(tasks[i]);
+}
+
+function markAsDone(event) {
+  if (event.target.classList.value === "done") {
+    event.target.classList.remove("done");
+  } else {
+    event.target.classList.add("done");
+  }
+}
+
+function createList(event) {
+  var todo = document.createElement("li");
+  todo.textContent = event;
+  todoList.appendChild(todo);
+  todo.addEventListener("click", markAsDone);
+}
+
+var form = document.getElementsByTagName("form")[0];
+form.addEventListener("keypress", function(event) {
+  if (event.charCode === 13) {
+    event.preventDefault();
+    createList(event.target.value);
+  }
+});
+
+var btnPressed = document.getElementsByTagName("form")[0];
+btnPressed.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var userInput = form.elements["todo-input"].value;
+  if (userInput !== "") {
+    createList(userInput);
+    form.elements["todo-input"].value = "";
+  }
+});
